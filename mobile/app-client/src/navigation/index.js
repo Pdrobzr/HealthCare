e6c8bb36747f3b8cd80bd1d9cdd1de2c1dfc6b43
azPeu <psilvaoliveira122@gmail.com>
2023-09-11 19:08:13 -0300

    Modify navbar

--- mobile/app-client/src/navigation/index.js
+++ mobile/app-client/src/navigation/index.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //Screens
@@ -10,39 +10,59 @@ import HomeScreen from './pages/Home';
 import ProfileScreen from './pages/Profle';
 import MapsScreen from './pages/Maps';
 
+//CustomTab
+import CustomTabBar from '../components/CustomTabBar';
 //Screens names
 
 const homeName = 'Home';
 const profileName = 'Configurações';
 const mapsName = 'Mapa';
 
+
+
+
 const Tab = createBottomTabNavigator();
-export default function MainContainer(){
-    return(
-       <NavigationContainer>
-        <Tab.Navigator initialRouteName={homeName} screenOptions={({route}) => ({
-            tabBarIcon: ({focused,color,size}) => {
-                let iconName;
-                let rn = route.name;
-
-                if(rn === homeName){
-                    iconName = focused ? 'home' : 'home-outline'
-                }else if (rn === mapsName){
-                    iconName = focused ? 'location' : 'location-outline'
-                }else if (rn === profileName){
-                    iconName = focused ? 'settings' : 'settings-outline'
+export default function MainContainer() {
+    return (
+        <NavigationContainer >
+            <Tab.Navigator initialRouteName={homeName} screenOptions={{
+                headerShown: false,
+                tabBarHideOnKeyboard: true,
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: 'rgba(0, 112, 239, 0.8)',
+                tabBarStyle: {
+                    borderTopWidth: 0,
+                    backgroundColor: "#FFF"
                 }
-                
-                return <Ionicons name={iconName} size={size} color={color}/>
+            }
 
-            },
-        })} >
-     
-        <Tab.Screen name={mapsName} component={MapsScreen}/>
-        <Tab.Screen name={homeName} component={HomeScreen}/>
-        <Tab.Screen name={profileName} component={ProfileScreen}/>
 
-        </Tab.Navigator>
-       </NavigationContainer>
+            } tabBar={(props) => <CustomTabBar {...props} />}>
+
+                <Tab.Screen
+                    name={mapsName} 
+                    component={MapsScreen}
+                    options={{
+                        tabBarIcon: "location"
+                    }} 
+                 />
+                <Tab.Screen 
+                    name={homeName} 
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: "home"
+                    }}  
+                />
+                <Tab.Screen 
+                    name={profileName} 
+                    component={ProfileScreen}
+                    options={{
+                        tabBarIcon: "settings"
+                    }}  
+                />
+
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
+
