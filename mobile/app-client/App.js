8d4074de69e791333b286ac16626886453c2c21c
azPeu <psilvaoliveira122@gmail.com>
2023-10-24 06:44:11 -0300

    Update autentication

--- mobile/app-client/App.js
+++ mobile/app-client/App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import MainContainer from './src/navigation';
 import Login from './src/authentication/Login/index'
-
+import Register from './src/authentication/Register/index'
 function App(){
   return(
-    <Login/>
+    <Register/>
   )
 }
 
--- mobile/app-client/src/authentication/Login/index.js
+++ mobile/app-client/src/authentication/Login/index.js
@@ -1,115 +1,108 @@
 
-import { View,Text,StyleSheet ,TouchableOpacity,Platform, Pressable,TextInput, Image} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Platform, Pressable, TextInput, Image } from 'react-native';
 import * as React from 'react';
-import {useFonts} from 'expo-font';
-import { FloatingTitleTextInputField } from '../../components/CustomInputs/index';
+import { useFonts } from 'expo-font';
+import Input from '../../components/CustomInputs/index';
 
-export default function Login(){
+export default function Login() {
     const [fontsLoaded] = useFonts({
         'Montserrat-Bold': require('../../components/fonts/Montserrat-Bold.ttf'),
         'Montserrat-SemiBold': require('../../components/fonts/Montserrat-SemiBold.ttf'),
         'Montserrat-Medium': require('../../components/fonts/Montserrat-Medium.ttf'),
     })
-    if(!fontsLoaded){
+    if (!fontsLoaded) {
         return undefined;
     }
 
-    state = {
-        usuario: '',
-        senha: '',
-      }
-    
-      _updateMasterState = (attrName, value) => {
-        this.setState({ [attrName]: value });
-      }
 
-    return(
+    return (
         <View style={styles.container}>
-           <View style={styles.image}>
+            <View style={styles.image}>
                 <Image
-                     style={styles.imgLogo}
-                     source={require('../../../assets/logo.png')}
-                 />
+                    style={styles.imgLogo}
+                    source={require('../../../assets/logo.png')}
+                />
             </View>
-          
-           <View style={styles.formsInput}>
-           <View style={styles.contentTitle}>
-                <Text style={styles.subTitle}>Prossiga com o seu</Text>
-                <Text style={styles.titleText}>Login</Text>
-            </View>
-           <FloatingTitleTextInputField 
-                attrName = 'usuario'
-                title = 'Usuario'
-                value = {this.state.lastName}
-                updateMasterState = {this._updateMasterState}
-            />
-           <FloatingTitleTextInputField 
-           style={styles.formsInputText}
-                attrName = 'senha'
-                title = 'Senha'
-                value = {this.state.lastName}
-                updateMasterState = {this._updateMasterState}
-            />
+
+            <View style={styles.formsInput}>
+                <View style={styles.contentTitle}>
+                    <Text style={styles.subTitle}>Prossiga com o seu</Text>
+                    <Text style={styles.titleText}>Login</Text>
+                </View>
+                <Input placeholder="Usuario" iconName="user" iconSize={24} />
+                <Input secureTextEntry={true} placeholder="Senha" iconName="lock" iconSize={24} />
                 <Pressable style={styles.buttonLogin}>
-                     <Text style={styles.formsButtonText}>LOGIN</Text>
-                </Pressable> 
-           </View>
-           
-          
+                    <Text style={styles.formsButtonText}>LOGIN</Text>
+                </Pressable>
+                <Text style={styles.subsTitle}>Não tem uma conta ?
+                    <Text style={{color:'#1387F1'}}> Cadastre-se</Text>
+                </Text>
+                <Text style={styles.subsTitle}>Esqueceu sua senha ?
+                    <Text style={{color:'#1387F1'}}> Recupere aqui</Text>
+                </Text>
+            </View>
+
+
         </View>
     )
 }
 
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
-        flex:1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        flexDirection:'column',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        flexDirection: 'column',
         justifyContent: 'nowrap',
     },
-    image:{
-        flex:1,
-        top:50,
+    image: {
+        flex: 1,
+        top: 50,
     },
     imgLogo: {
-        width:360, 
+        width: 360,
         height: 300,
         alignSelf: 'center',
-        
+
     },
-    contentTitle:{
-        flex:0.2,
-        alignItems:'flex-start',
+    contentTitle: {
+        right: 80,
+        bottom: 40,
     },
-    subTitle:{
-        fontFamily:"Montserrat-Medium",
-        fontSize:20,
-        color:'#4A4444'
+    subTitle: {
+        fontFamily: "Montserrat-Medium",
+        fontSize: 20,
+        color: '#4A4444'
     },
-    titleText:{
-        fontFamily:"Montserrat-Bold",
-        color:'#48BBC6',
+    titleText: {
+        fontFamily: "Montserrat-Bold",
+        color: '#48BBC6',
         fontSize: 50,
     },
-    formsInput:{
-        flex:2,
+    formsInput: {
+        bottom: 21,
+        flex: 1,
+        alignItems: 'center'
     },
 
-    buttonLogin:{
+    buttonLogin: {
         alignSelf: 'center',
         textAlign: 'center',
         width: 350,
         height: 50,
         margin: 10,
         backgroundColor: '#6CA8DA',
-        alignItems:'center',
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    formsButtonText:{
-        color:'#fff',
-        fontFamily:"Montserrat-SemiBold",
-        fontSize:30,
-    }
+    formsButtonText: {
+        color: '#fff',
+        fontFamily: "Montserrat-SemiBold",
+        fontSize: 30,
+    },
+    subsTitle:{
+        color:'#3E3C3C',
+        
+    },
 })
