ac7fe3bdffdfe5666e9cdb1845e0479d5b3e1276
Pedro Bezerra <pedrohbs67@hotmail.com>
2023-11-20 15:36:40 -0300

    Mudando o formato das datas

--- backend/src/controllers/UsuarioController.ts
+++ backend/src/controllers/UsuarioController.ts
@@ -157,11 +157,33 @@ export class UsuarioController {
         const idUsuario = Number(req.query.idUsuario);
         const idEmpresa = Number(req.query.idEmpresa);
 
+        function obterDataEHoraBrasil() {
+            // Obtém a data e hora atuais no fuso horário do sistema local
+            const dataEHoraAtual = new Date();
+            
+            // Extrai os componentes da data e hora
+            const dia = String(dataEHoraAtual.getDate()).padStart(2, '0');
+            const mes = String(dataEHoraAtual.getMonth() + 1).padStart(2, '0');
+            const ano = dataEHoraAtual.getFullYear();
+            const hora = String(dataEHoraAtual.getHours()).padStart(2, '0');
+            const minuto = String(dataEHoraAtual.getMinutes()).padStart(2, '0');
+            const segundo = String(dataEHoraAtual.getSeconds()).padStart(2, '0');
+          
+            // Cria a string no formato desejado (DD/MM/YYYY HH:mm:ss)
+            const dataEHoraFormatadas = `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`;
+          
+            return dataEHoraFormatadas;
+          }
+
+          const dataEHoraBrasil = obterDataEHoraBrasil().toString();
+
+
         const realizarComentario = await prisma.comentario.create({
             data: {
                 conteudoComentario: conteudo,
                 idUsuario: idUsuario,
-                idEmpresa: idEmpresa,              
+                idEmpresa: idEmpresa,   
+                dataPublicacao: dataEHoraBrasil           
             }
         });
 
