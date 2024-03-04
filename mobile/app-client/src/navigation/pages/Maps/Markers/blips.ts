e4c937c31e035176c07469293b8a0457db093f78
azPeu <psilvaoliveira122@gmail.com>
2024-03-04 12:44:32 -0300

    .

--- mobile/app-client/src/navigation/pages/Maps/Markers/blips.ts
+++ mobile/app-client/src/navigation/pages/Maps/Markers/blips.ts
@@ -2,22 +2,17 @@ export const blips = [
     {
         latitude: -24.0047,
         longitude: -46.4189,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
-        name: "Complexo Hospitalar Irmã Dulce",
+        name: 'Complexo Hospitalar Irmã Dulce',
     },
     {
         latitude: -24.0056,
         longitude: -46.4241,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
         name: "Pronto Socorro Central de Praia Grande",
     },
     {
         latitude: -24.0130,
         longitude: -46.4459,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
+ 
         name: "Hospital Ana Cosata de Praia Grande",
     },
 
--- mobile/app-client/src/navigation/pages/Maps/index.js
+++ mobile/app-client/src/navigation/pages/Maps/index.js
@@ -2,71 +2,116 @@ import * as React from 'react';
 
 import { useState, useEffect } from 'react';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, Platform } from 'react-native';
 import * as Location from 'expo-location';
-
-
+import { blips } from './Markers/blips';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function MapsScreen({ navigation }) {
 
   const [location, setLocation] = useState(null);
   const [initialRegion, setInitialRegion] = useState(null);
+
+ 
   useEffect(() => {
     (async () => {
 
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        //setErrorMsg('Permission to access location was denied');
         return;
       }
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location.coords);
-      console.log("Localização",location.coords)
-
+   
 
       setInitialRegion({
-        latitude: location.coords.latitude,
-        longitude:location.coords.longitude,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
+        
       });
     })();
 
     
   }, []);
 
- //let text = 'Waiting..';
- //if (errorMsg) {
- //  text = errorMsg;
- //} else if (location) {
- //  text = JSON.stringify(location);
- //}
+  const mapStyle = [
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ]
 
   return (
     <View style={styles.container}>
-
+      <View style={styles.searchBar}></View>
         <MapView
         style={StyleSheet.absoluteFill}
         initialRegion={{
           latitude: -23.9991,
-          longitude: -46.4133,
+          longitude:-46.4133,
           latitudeDelta: 0.005,
           longitudeDelta: 0.005,
         }}
         provider={PROVIDER_GOOGLE}
-        showsMyLocationButton>
-          ()
-          {location && (
-                  <Marker
-                  coordinate={{
-                    latitude: -23.9991,
-                    longitude: -46.4133,
-                  }}
-                  title='Sua localização'
-                  ></Marker>
-          )}
+        showsMyLocationButton
+        customMapStyle={mapStyle}
+        >
+           {blips.map((blips, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude:blips.latitude,
+                longitude:blips.longitude
+              }}
+              title={blips.name}
+        
+            />
+          ))}
+          
         </MapView>
+        <View style={styles.searchBox}>
+              <TextInput
+                placeholder='Pesquisar'
+                placeholderTextColor='#000'
+                autoCapitalize='none'
+                style={{flex:1,padding: 0}}
+              />
+                <FontAwesome
+                   name="search"
+                   size={15}
+                   color={'#000'}
+                />
+        </View>
     </View>
   );
 }
@@ -75,5 +120,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  searchBox:{
+    position:'absolute',
+    marginTop: Platform.OS == 'ios' ? 40 : 40,
+    flexDirection:'row',
+    alignItems:'center',
+    backgroundColor:'#fff',
+    width:'90%',
+    alignSelf:'center',
+    borderRadius:5,
+    padding: 10,
+    shadowColor:'#ccc',
+    shadowOffset: {widht: 0,height: 3},
+    shadowOpacity: 0.5,
+    shadowRadius:5,
+    elevation:10,
+  }
 
 });
